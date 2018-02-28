9eae51d05feb6446ea03235fc1a873ccd27df278
Ralf Yang <goody80762@gmail.com>
2018-02-28 11:17:05 +0900

    update for remote install with git

--- remote-install.sh
+++ remote-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -sL https://raw.githubusercontent.com/goody80/vagrant_docker_cluster/master/ctl.sh -o ./ctl.sh
+git clone https://github.com/goody80/vagrant_docker_cluster.git
+#curl -sL https://raw.githubusercontent.com/goody80/vagrant_docker_cluster/master/ctl.sh -o ./ctl.sh
 chmod 755 ./ctl.sh
 ./ctl.sh
