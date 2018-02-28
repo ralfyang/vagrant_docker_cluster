7cc09a5f4de91fa04ccd21bd753417aa61252e44
Ralf Yang <goody80762@gmail.com>
2018-02-28 11:25:18 +0900

    purge the old script

--- remote-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/goody80/vagrant_docker_cluster.git
-#curl -sL https://raw.githubusercontent.com/goody80/vagrant_docker_cluster/master/ctl.sh -o ./ctl.sh
-cd vagrant_docker_cluster
-chmod 755 ./ctl.sh
-./ctl.sh
