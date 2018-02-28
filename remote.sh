d433c7ca7a89c323a315043e4e23ff1ed8633794
Ralf Yang <goody80762@gmail.com>
2018-02-28 11:22:57 +0900

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-curl -sL https://raw.githubusercontent.com/goody80/vagrant_docker_cluster/master/ctl.sh -o ./ctl.sh
+git clone https://github.com/goody80/vagrant_docker_cluster.git
+#curl -sL https://raw.githubusercontent.com/goody80/vagrant_docker_cluster/master/ctl.sh -o ./ctl.sh
+cd vagrant_docker_cluster
 chmod 755 ./ctl.sh
 ./ctl.sh
