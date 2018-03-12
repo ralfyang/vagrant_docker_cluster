4dae3534b50a6d92c96fa28765676f6d6696987d
Ralf Yang <goody80762@gmail.com>
2018-03-12 15:07:14 +0900

    Update ctl.sh

--- ctl.sh
+++ ctl.sh
@@ -47,23 +47,40 @@ remove(){
 application_install(){
 	mkdir -p ~/tmp
 	cd ~/tmp
-	## VirtualBox Download & Install
-	VirtualBox_installer="http://download.virtualbox.org/virtualbox/5.2.2/VirtualBox-5.2.2-119230-OSX.dmg"
-	VirtualBox_file=$(echo "$VirtualBox_installer" | awk -F'/' '{print $NF}')
-	wget $VirtualBox_installer
-	sudo hdiutil attach $VirtualBox_file
-	sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /
-	hdiutil unmount /Volumes/VirtualBox/
-	rm -f ./$VirtualBox_file
+  	arch=`uname -s`-`uname -m`
+	os=`uname -s`
 
-	## Vagrant Download & Install
-	Vagrant_installer="https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.dmg"
-	Vagrant_file=$(echo "$Vagrant_installer" | awk -F'/' '{print $NF}')
-	wget $Vagrant_installer
-	sudo hdiutil attach $Vagrant_file
-	sudo installer -pkg /Volumes/Vagrant/vagrant.pkg -target /
-	hdiutil unmount /Volumes/Vagrant
-	rm -f ./$Vagrant_installer
+  	case $os in
+    	Linux)
+      		## VirtualBox install
+      		sudo apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
+      		wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+      		sudo apt-get update
+      		sudo apt-get install virtualbox  -y
+   		;;
+
+    	Darwin)
+            	## VirtualBox Download & Install
+            	VirtualBox_installer="http://download.virtualbox.org/virtualbox/5.2.2/VirtualBox-5.2.2-119230-OSX.dmg"
+            	VirtualBox_file=$(echo "$VirtualBox_installer" | awk -F'/' '{print $NF}')
+            	wget $VirtualBox_installer
+            	sudo hdiutil attach $VirtualBox_file
+            	sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /
+            	hdiutil unmount /Volumes/VirtualBox/
+            	rm -f ./$VirtualBox_file
+
+		## Vagrant Download & Install
+		Vagrant_installer="https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.dmg"
+		Vagrant_file=$(echo "$Vagrant_installer" | awk -F'/' '{print $NF}')
+		wget $Vagrant_installer
+		sudo hdiutil attach $Vagrant_file
+		sudo installer -pkg /Volumes/Vagrant/vagrant.pkg -target /
+		hdiutil unmount /Volumes/Vagrant
+		rm -f ./$Vagrant_installer
+	    	;;
+    	esac
+
+
 }
 
 clear
