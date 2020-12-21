93948df8fe01c783fbde16fb829081283b1faf8a
Ralf Yang <goody80762@gmail.com>
2020-12-21 12:51:22 +0900

    update for provisioning script

--- provisioning/docker.sh
+++ provisioning/docker.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+LANG=en_US.UTF-8
+#sed -i '/^LANG=/d' /etc/sysconfig/i18n
+#echo 'LANG=en_US.UTF-8' >> /etc/sysconfig/i18n
+#sed -i 's/=enforcing/=disabled/g' /etc/selinux/config
+
+#setenforce 0
+apt update
+apt-get install curl wget net-tools -y
+
+mkdir -p /root/.ssh
+cp -Rfv /tmp/conf.d/* /root/.ssh
+sudo -u vagrant echo "alias ll='ls -lia' " >> /home/vagrant/.bash_profile
+
 echo ">> Docker Install"
 curl -sL zxz.kr/docker|bash
